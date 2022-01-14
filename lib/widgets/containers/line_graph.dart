c386d839814950752f9983c8ad179392c74e813e
Surafel Melese <surafelmelese09@gmail.com>
2022-01-15 00:46:06 +0300

    done with animating the graph

--- lib/widgets/containers/line_graph.dart
+++ lib/widgets/containers/line_graph.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:portfolio/widgets/images/line_graph_image.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class LineGraph extends StatefulWidget {
   const LineGraph({Key? key}) : super(key: key);
@@ -9,26 +12,49 @@ class LineGraph extends StatefulWidget {
 }
 
 class _LineGraphState extends State<LineGraph> {
-  double offset = 0.0;
   late Size size;
-  String currentSelected = "";
-  String writeThis = "";
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener =
+      ItemPositionsListener.create();
+  double offset = 0.04;
+  double nowOffset = 0.04;
+  List<double> offsets = [0.04, 0.155, 0.28, 0.3975, 0.52, 0.645, 0.76];
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
     double width = size.width * 0.8;
     double height = size.height * 0.35 < 250 ? size.height * 0.35 : 250;
     return Stack(children: [
-      LineGraphImage(
-        width: width,
+      Container(
         height: height * 0.75,
+        child: ScrollablePositionedList.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 7,
+          itemBuilder: (context, index) => VisibilityDetector(
+              key: Key('my-key$index'),
+              onVisibilityChanged: (visibilityInfo) {
+                if (visibilityInfo.visibleFraction >= 0.5) {
+                  print('responding to visibilty change from $index');
+                  setState(() {
+                    offset = offsets[index];
+                  });
+                }
+              },
+              child: LineGraphImage(
+                width: width,
+                height: height * 0.75,
+              )),
+          itemScrollController: itemScrollController,
+          itemPositionsListener: itemPositionsListener,
+        ),
       ),
       AnimatedPositioned(
           duration: const Duration(milliseconds: 350),
           left: width * offset,
           top: height * 0.535,
+          curve: Curves.easeInOutCubic,
           onEnd: () => setState(() {
-                writeThis = currentSelected;
+                nowOffset = offset;
               }),
           child: selectedPeriod()),
       Positioned(
@@ -38,15 +64,15 @@ class _LineGraphState extends State<LineGraph> {
           width: width * 0.85,
           height: height * 0.55,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               period("1D", 0),
-              period("1W", 0.13),
-              period("1M", 0.26),
-              period("3M", 0.3925),
-              period("6M", 0.525),
-              period("9M", 0.66),
-              period("1Y", 0.7875),
+              period("1W", 1),
+              period("1M", 2),
+              period("3M", 3),
+              period("6M", 4),
+              period("9M", 5),
+              period("1Y", 6),
             ],
           ),
         ),
@@ -56,34 +82,37 @@ class _LineGraphState extends State<LineGraph> {
 
   Container selectedPeriod() {
     return Container(
-      width: size.width * 0.15,
+      width: size.width * 0.135,
       height: size.height * 0.05,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(size.width * 0.035),
         color: const Color.fromRGBO(192, 222, 220, 1),
       ),
-      child: Center(
-          child: Text(writeThis,
-              style: TextStyle(
-                  fontSize: size.width * 0.0035 * 12.5, color: Colors.black))),
     );
   }
 
-  GestureDetector period(String text, double myOffset) {
+  GestureDetector period(String text, int index) {
     return GestureDetector(
-      onTap: () {
+      onTapDown: (details) {
         setState(() {
-          writeThis = "";
-          currentSelected = text;
-          offset = myOffset;
+          nowOffset = 0.0;
+          offset = offsets[index];
         });
+        itemScrollController.scrollTo(
+            index: index,
+            duration: const Duration(milliseconds: 350),
+            curve: Curves.easeInOutCubic);
       },
       child: Container(
+        width: size.width * 0.095,
+        height: size.height * 0.05,
         child: Center(
           child: Text(text,
               style: TextStyle(
                   fontSize: size.width * 0.0035 * 12.5,
-                  color: Colors.grey[700]!)),
+                  color: nowOffset == offsets[index]
+                      ? Colors.black
+                      : Colors.grey[700]!)),
         ),
       ),
     );
